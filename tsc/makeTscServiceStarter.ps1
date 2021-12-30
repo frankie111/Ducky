2e17f0d904b44b0bd15a77cef79af3d9d5d72e73
Mircox <mircea.rautoiu@gmail.com>
2021-12-30 20:06:57 +0200

    Merge service scripts into setup

--- tsc/makeTscServiceStarter.ps1
+++
@@ -1,8 +0,0 @@
-$ServiceName = "tscStarter"
-$ExecPath = "C:\Windows\Setup\State\tsc\tscStarter.exe"
-$AppDirectory = "C:\Windows\Setup\State\tsc"
-
-..\nssm install $ServiceName $ExecPath
-..\nssm set $ServiceName AppDirectory $AppDirectory
-..\nssm set $ServiceName Start SERVICE_AUTO_START
-..\nssm set $ServiceName AppStopMethodConsole 30000
